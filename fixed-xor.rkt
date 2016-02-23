b04eaec6968ac415e8071c6f951f9c22bee88488
Joe Ardent <code@ardent.nebcorp.com>
2016-02-23 17:57:13 -0500

    tests for challenge 2 pass

---
+++ fixed-xor.rkt
@@ -0,0 +1,13 @@
+#lang racket
+
+(provide xor)
+
+(require "hex-utils.rkt")
+
+(define (xor s1 s2)
+  (let [(bs1 (hex->bytes s1))
+        (bs2 (hex->bytes s2))]
+    (apply ~a (map (λ (x) (number->string x 16))
+                   (for/list [(i bs1)
+                              (j bs2)]
+                     (bitwise-xor i j))))))
--- test.rkt
+++ test.rkt
@@ -1,27 +1,42 @@
 #lang racket
 
 (require "hex-utils.rkt")
+(require "fixed-xor.rkt")
 
 (module+ test
   (require rackunit rackunit/text-ui)
 
-       (define base64-string #"SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t")
-     (define hex-string "49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d")
-     (define byte-string #"I'm killing your brain like a poisonous mushroom")
+  ;; challenge 1
+  (define base64-string #"SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t")
+  (define hex-string "49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d")
+  (define c1-byte-string #"I'm killing your brain like a poisonous mushroom")
+
+  ;; challenge 2
+  (define c2-input "1c0111001f010100061a024b53535009181c")
+  (define c2-test-val "686974207468652062756c6c277320657965")
+  (define c2-output "746865206b696420646f6e277420706c6179")
+  (define c2-out-decoded #"the kid don't play")
 
   (define suite
     (test-suite
-     "hex tests"
+     "challenges"
 
-
+     ;; challenge 1
      (test-equal? "base64"
                   (hex->base64 hex-string)
                   base64-string)
 
      (test-equal? "ascii to bytes"
                   (hex->bytes hex-string)
-                  byte-string)
+                  c1-byte-string)
 
-))
+     (test-equal? "kid don't play"
+                  (hex->bytes c2-output)
+                  c2-out-decoded)
+
+     ;; challenge 2
+     (test-equal? "xor"
+                  (xor c2-input c2-test-val)
+                  c2-output)))
 
   (run-tests suite))

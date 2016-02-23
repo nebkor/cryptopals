b30dd441733ee345c1653aa242f3ffbbe85b6a04
Joe Ardent <code@ardent.nebcorp.com>
2016-02-23 16:30:50 -0500

    Finished first challenge.

---
+++ 1.rkt
@@ -0,0 +1,26 @@
+#lang racket
+
+(provide hex->base64 hex->bytes)
+
+(require net/base64)
+
+;; 49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d
+;; should produce
+;; SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t
+
+;;(define hstring "49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d") ;; "I'm killing your brain like a poisonous mushroom"
+
+(define (hex->bytes s)
+  (define (iter sl r)
+    (if (null? sl)
+        (list->bytes (reverse r))
+        (let* [(f (car sl))
+               (s (cadr sl))
+               (b (~a f s))
+               (bb (string->number b 16))
+               (rest (cddr sl))]
+          (iter rest (cons bb r)))))
+  (iter (string->list s) '()))
+
+(define (hex->base64 s)
+  (base64-encode (hex->bytes s)))
